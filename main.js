434f7b7b08e19e5fee8e8c2016ef7f73b2998bcd
Michele_Manco <motlamik@gmail.com>
2019-11-28 02:33:32 +0100

    Carousel done!!!

--- main.js
+++ main.js
@@ -22,3 +22,64 @@ $('.prev').click(function(){
  img_corrente.removeClass('active');
  img_precedente.addClass('active');
 });
+
+
+
+// $('.next').click(function(){
+//  var img_corrente = $('img.active');
+//  var dot_corrente = $('i.active');
+//  var img_successiva = img_corrente.next('img');
+//  var dot_successivo = dot_corrente.next('i');
+//  if (img_successiva.length == 0) {
+//    img_successiva = $('.slider img:first-child');
+//  }
+//  img_corrente.removeClass('active');
+//  img_successiva.addClass('active');
+//  if (dot_successivo.length == 0) {
+//    dot_successivo = $('.slider i:first-child');
+//  };
+//  dot_corrente.removeClass('active');
+//  dot_successivo.addClass('active');
+//
+// });
+// $('.prev').click(function(){
+//  var img_corrente = $('img.active');
+//  var dot_corrente = $('i.active');
+//  var img_precedente = img_corrente.prev('img');
+//  var dot_precedente = dot_corrente.prev('i');
+//  if (img_precedente.length == 0) {
+//    img_precedente = $('.slider img:last-child');
+//   }
+//  img_corrente.removeClass('active');
+//  img_precedente.addClass('active');
+//  if (dot_precedente.length == 0) {
+//    dot_precedente = $('.slider i:last-child');
+//   }
+//  dot_corrente.removeClass('active');
+//  dot_precedente.addClass('active');
+// });
+$('.next').click(function(){
+
+ var dot_corrente = $('i.active');
+
+ var dot_successivo = dot_corrente.next('i');
+
+ if (dot_successivo.length == 0) {
+   dot_successivo = $('.bullett i:first-child');
+ }
+ dot_corrente.removeClass('active');
+ dot_successivo.addClass('active');
+
+});
+$('.prev').click(function(){
+
+ var dot_corrente = $('i.active');
+
+ var dot_precedente = dot_corrente.prev('i');
+
+ if (dot_precedente.length == 0) {
+   dot_precedente = $('.bullett i:last-child');
+  }
+ dot_corrente.removeClass('active');
+ dot_precedente.addClass('active');
+});
